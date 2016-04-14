d0a02370010645fda963c57a8506c658c4a131fd
Lovinder Pnag <lspnag@gmail.com>
2016-04-13 22:46:04 -0400

    added route and show method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :garages
+
+  get 'search', to: 'search#search'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
