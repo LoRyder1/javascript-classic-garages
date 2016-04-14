c824f0cee3d658ab6dec21a24a89ea9378678019
Lovinder Pnag <lspnag@gmail.com>
2016-04-14 01:07:02 -0400

    highlighting with search

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -3,6 +3,44 @@ require 'elasticsearch/model'
 class Garage < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :name, analyzer: 'english', index_options: 'offsets'
+      indexes :car_type, analyzer: 'english'
+    end
+  end
+
+  def self.search query
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query, 
+            fields: ['name^10', 'car_type']
+          }
+        },
+        highlight: {
+          pre_tags: ['<em>'],
+          post_tags: ['</em>'],
+          fields: {
+            name: {},
+            car_type: {}
+          }
+        }
+      }
+    )
+  end
 end
+
+
+  Garage.__elasticsearch__.client.indices.delete index: Garage.index_name rescue nil
+
+  Garage.__elasticsearch__.client.indices.create \
+    index: Garage.index_name,
+    body: { settings: Garage.settings.to_hash, mappings: Garage.mappings.to_hash }
+
+
+  Garage.import
 
 # Garage.import
