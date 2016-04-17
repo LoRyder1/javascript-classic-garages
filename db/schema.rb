832b09d81b74b86365cb1fac864a80e21e9a0743
Lovinder Pnag <lspnag@gmail.com>
2016-04-16 23:47:10 -0400

    new users controller and table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411194750) do
+ActiveRecord::Schema.define(version: 20160417034522) do
 
   create_table "garages", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20160411194750) do
     t.float    "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "display_name"
+    t.string   "email"
+    t.string   "password_hash"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
