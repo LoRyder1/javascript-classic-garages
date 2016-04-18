660739b5fb1a78667b3985d0952d71c87b5a90a0
Lovinder Pnag <lspnag@gmail.com>
2016-04-17 22:30:19 -0400

    new user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to timeentries_path
+    else
+      render 'new'
+    end
+  end
+
+  def doc
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+  def redirect_if_logged_in
+    redirect_to root_path if logged_in?
+  end
 end
