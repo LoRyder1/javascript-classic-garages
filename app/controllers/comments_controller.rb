b47673144ea76265850c4e84dc8ab558314e5be1
Lovinder Pnag <lspnag@gmail.com>
2016-04-30 23:25:21 -0400

    create coments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  
+  def create
+    @car = Garage.find(paramd[:garage_id])
+    @comment = @car.comments.create(comment_params)
+    redirect_to garage_path(@car)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+  
 end
