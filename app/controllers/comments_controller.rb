9e367a04434c02f81601495f4eaa1af56e17401a
Lovinder Pnag <lspnag@gmail.com>
2016-04-30 23:31:13 -0400

    fixed typo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   
   def create
-    @car = Garage.find(paramd[:garage_id])
+    @car = Garage.find(params[:garage_id])
     @comment = @car.comments.create(comment_params)
     redirect_to garage_path(@car)
   end
