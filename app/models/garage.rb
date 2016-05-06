64f029353ff8ba68715d73763a424ca3c0a444d1
Lovinder Pnag <lspnag@gmail.com>
2016-05-05 23:06:36 -0400

    ensure slug uniqueness

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -3,6 +3,17 @@ require 'elasticsearch/model'
 class Garage < ActiveRecord::Base
   has_many :comments
 
+  def slug
+    title.downcase.gsub(" ", "-")
+  end
+
+  def to_param
+    "#{id}-#{slug}"
+  end
+
+
+
+
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
 
@@ -33,6 +44,25 @@ class Garage < ActiveRecord::Base
       }
     )
   end
+
+  protected
+
+  # validate
+  def ensure_slug_uniqueness
+    
+    # we also wnat to ensure teh slug is not blank
+    if self.slug.blank?
+      errors.add(:slug, "can't be blank")
+    end
+
+    # if this is a new article, the id is nil
+    # otherwise, the slug should point to this article's id
+    unless Slug[self.slug].nil? || Slug[self.slug] == self.id.to_s
+      errors.add(:slug, "is already taken")
+    end
+  end
+
+
 end
 
 
