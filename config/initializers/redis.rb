ccf1cff179c2b1cb584e4ea7a168aff61bac2b8a
Lovinder Pnag <lspnag@gmail.com>
2016-05-05 22:53:56 -0400

    connect to server and store as global resource

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,3 @@
-$redis = Redis.new(host: 'localhost', port: 3000)
+$redis = Redis::Namespace.new("orange-blog", :redis => Redis.new)
+
+# connect to the server and store that connection as a global resource
