c17a86d00c19c21af11e5dad6db93e27e53909fb
Lovinder Pnag <lspnag@gmail.com>
2016-12-15 09:21:21 -0800

    logging in with authy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,30 @@ class SessionsController < ApplicationController
       flash[:error] = "Invalid email or password"
       render 'new'
     end
+
+    # Twilio api
+
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:pre_2fa_auth_user_id] = @user.id
+
+      #Try to verify with OneTouch
+      one_touch = Authy::OneTouch.send_approval_request(
+        id: @user.authy_id,
+        message: "Request to Login to Twilio demo app",
+        deatils: {
+          'Email Address' => @user.email,
+        }
+      )
+      status = one_touch['success'] ? :onetouch : :sms
+      @user.update(authy_status: status)
+
+      # Respond to the ajax call that requested this with approval request body
+      render json: { success: one_touch['success'] }
+    else
+      @user ||= User.new(email: params[:email])
+      render :new
+    end
   end
 
   def destroy
