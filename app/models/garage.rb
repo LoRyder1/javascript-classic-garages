771f7aa732092a2340cdbef4b171f7d32c6067d0
Lovinder Pnag <lspnag@gmail.com>
2016-12-19 09:28:25 -0800

    readd elastic search

--- app/models/garage.rb
+++ app/models/garage.rb
@@ -11,36 +11,36 @@ class Garage < ActiveRecord::Base
     "#{id}-#{slug}"
   end
 
-  # include Elasticsearch::Model
-  # include Elasticsearch::Model::Callbacks
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 
-  # settings index: { number_of_shards: 1 } do
-  #   mappings dynamic: 'false' do
-  #     indexes :name, analyzer: 'english', index_options: 'offsets'
-  #     indexes :car_type, analyzer: 'english'
-  #   end
-  # end
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :name, analyzer: 'english', index_options: 'offsets'
+      indexes :car_type, analyzer: 'english'
+    end
+  end
 
-  # def self.search query
-  #   __elasticsearch__.search(
-  #     {
-  #       query: {
-  #         multi_match: {
-  #           query: query, 
-  #           fields: ['name^10', 'car_type']
-  #         }
-  #       },
-  #       highlight: {
-  #         pre_tags: ['<em>'],
-  #         post_tags: ['</em>'],
-  #         fields: {
-  #           name: {},
-  #           car_type: {}
-  #         }
-  #       }
-  #     }
-  #   )
-  # end
+  def self.search query
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query, 
+            fields: ['name^10', 'car_type']
+          }
+        },
+        highlight: {
+          pre_tags: ['<em>'],
+          post_tags: ['</em>'],
+          fields: {
+            name: {},
+            car_type: {}
+          }
+        }
+      }
+    )
+  end
 
   protected
 
@@ -63,11 +63,11 @@ class Garage < ActiveRecord::Base
 end
 
 
-  # Garage.__elasticsearch__.client.indices.delete index: Garage.index_name rescue nil
+  Garage.__elasticsearch__.client.indices.delete index: Garage.index_name rescue nil
 
-  # Garage.__elasticsearch__.client.indices.create \
-  #   index: Garage.index_name,
-  #   body: { settings: Garage.settings.to_hash, mappings: Garage.mappings.to_hash }
+  Garage.__elasticsearch__.client.indices.create \
+    index: Garage.index_name,
+    body: { settings: Garage.settings.to_hash, mappings: Garage.mappings.to_hash }
 
 
-  # Garage.import
+  Garage.import
